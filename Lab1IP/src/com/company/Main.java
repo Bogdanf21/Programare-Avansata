42878afb355e4ddd213f11b346edc4423f22a020
bogdanf21 <bogdanfomin21@gmail.com>
2022-02-21 09:39:16 +0200

    lab1

---
+++ Lab1IP/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("HELLO WORLD ");
+        Pacient pac1 = new Pacient("Popescu","boala1");
+        Pacient pac2 = new Pacient("Ionescu","boala2");
+        Pacient pac3 = new Pacient("Vasilescu","boala3");
+        Pacient pac4 = new Pacient("Petrescu","boala3");
+
+
+
+        Doctor doc1 = new Doctor("Popescu","ORL",null);
+        Doctor doc2 = new Doctor("Georgescu", "Inima",new Pacient[]{pac1,pac4});
+
+        doc1.addPacient(pac1);
+        doc1.addPacient(pac2);
+        doc1.addPacient(pac3);
+        doc1.printPacienti();
+        doc2.printPacienti();
+    }
+
+}
+
---
+++ Lab1IP/src/com/company/Pacient.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Pacient extends Person {
+    private String boala;
+    public Pacient(String nume,String boala)
+    {
+        this.nume = nume;
+        this.boala = boala;
+    }
+    public String getBoala(){return boala;}
+    public String getNume(){return nume;}
+}
