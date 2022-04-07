7f6bb87c48f84255ac889b121077822ad0ce6d51
bogdanf21 <bogdanfomin21@gmail.com>
2022-04-07 11:38:07 +0300

    rewritten

--- Homework6PA/src/main/java/MainFrame/DrawingPanel.java
+++ Homework6PA/src/main/java/MainFrame/DrawingPanel.java
@@ -8,36 +8,70 @@ import static sun.tools.jconsole.OutputViewer.init;
 
 
 public class DrawingPanel extends JPanel {
-    final MainFrame frame;
+    private final MainFrame frame;
+    int rows, cols;
     int canvasWidth = 400, canvasHeight = 400;
- ///...
-    BufferedImage image; //the offscreen image
-    Graphics2D offscreen; //the offscreen graphics
-
+    int boardWidth, boardHeight;
+    int cellWidth, cellHeight;
+    int padX, padY;
+    int stoneSize = 20;
 
     public DrawingPanel(MainFrame frame) {
-        this.frame = frame; createOffscreenImage(); init();
+        this.frame = frame;
+        init(frame.configPanel.getRows(), frame.configPanel.getCols());
     }
 
-
-    private void createOffscreenImage() {
-        image = new BufferedImage(
-                canvasWidth, canvasHeight, BufferedImage.TYPE_INT_ARGB);
-        offscreen = image.createGraphics();
-        offscreen.setColor(Color.WHITE); //fill the image with white
-        offscreen.fillRect(0, 0, canvasWidth, canvasHeight);
+    final void init(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.padX = stoneSize + 10;
+        this.padY = stoneSize + 10;
+        this.cellWidth = (canvasWidth - 2 * padX) / (cols - 1);
+        this.cellHeight = (canvasHeight - 2 * padY) / (rows - 1);
+        this.boardWidth = (cols - 1) * cellWidth;
+        this.boardHeight = (rows - 1) * cellHeight;
+        setPreferredSize(new Dimension(canvasWidth, canvasHeight));
     }
-    //...NEXT SLIDE
-    private void paintGrid() {
-        //same as before, only we draw in memory now
-        //only using offscreen instead of the original graphics
-    }
-    @Override
-    public void update(Graphics g) { } //No need for update
 
-    //Draw the offscreen image, using the original graphics
     @Override
     protected void paintComponent(Graphics graphics) {
-        graphics.drawImage(image, 0, 0, this);
+        Graphics2D g = (Graphics2D) graphics;
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, canvasWidth, canvasHeight);
+        paintGrid(g);
+        //paintSticks(g);
+        //paintStones(g);
+    }
+
+    private void paintGrid(Graphics2D g) {
+        g.setColor(Color.DARK_GRAY);
+        //horizontal lines
+        for (int row = 0; row < rows; row++) {
+            int x1 = padX;
+            int y1 = padY + row * cellHeight;
+            int x2 = padX + boardWidth;
+            int y2 = y1;
+            g.drawLine(x1, y1, x2, y2);
+        }
+
+        for(int column = 0;column < cols ;column++)
+        {
+            int x1 = padX + column * cellHeight;
+            int y1 = padY;
+            int x2 = x1;
+            int y2 = padY + boardHeight;
+            g.drawLine(x1, y1, x2, y2);
+
+        }
+        //intersections
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                int x = padX + col * cellWidth;
+                int y = padY + row * cellHeight;
+                g.setColor(Color.LIGHT_GRAY);
+                g.drawOval(x - stoneSize / 2, y - stoneSize / 2, stoneSize, stoneSize);
+            }
+        }
     }
 }
+
--- Homework6PA/src/main/java/MainFrame/MainFrame.java
+++ Homework6PA/src/main/java/MainFrame/MainFrame.java
@@ -5,11 +5,13 @@ package MainFrame;
 
 import javax.swing.*;
 
+import java.awt.*;
+
 import static javax.swing.SwingConstants.CENTER;
 
 public class MainFrame extends JFrame {
-  //  ConfigPanel configPanel;
-  //  ControlPanel controlPanel;
+    ConfigPanel configPanel;
+    ControlPanel controlPanel;
     DrawingPanel canvas;
 
     public MainFrame() {
@@ -19,23 +21,29 @@ public class MainFrame extends JFrame {
 
     private void init() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
         //create the components
+        this.configPanel = new ConfigPanel(this);
+        this.controlPanel = new ControlPanel(this);
+        this.canvas = new DrawingPanel(this);
+
       // canvas = new DrawingPanel(this);
-        JButton b = new JButton("Hello World");
-        b.setBounds(130,200,100,40);
 
-
- //...TODO
 
         //arrange the components in the container (frame)
-        //JFrame uses a BorderLayout by default
-        add(b, CENTER); //this is BorderLayout.CENTER
+        this.add(configPanel, BorderLayout.NORTH);
+        this.add(controlPanel, BorderLayout.SOUTH);
+        this.add(canvas,BorderLayout.CENTER);
+
+
+
+
+
         this.setSize(400,500);
 
  //...TODO
 
         //invoke the layout manager
+        this.setTitle("LAB6");
         pack();
     }
 
